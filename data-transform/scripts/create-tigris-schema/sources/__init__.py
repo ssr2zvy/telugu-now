c805fdfc06584715a9f695d4dfc19bf39b9163ce
ssr2zvy <146045765+ssr2zvy@users.noreply.github.com>
2026-09-11 15:00:35 +0000

    Configure deployment ignore policy

--- data-transform/scripts/create-tigris-schema/sources/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fleurs import read_fleurs
-from .indicvoices import read_indicvoices
-from .shrutilipi import read_shrutilipi
-
-__all__ = [
-    "read_fleurs",
-    "read_shrutilipi",
-    "read_indicvoices",
-]
